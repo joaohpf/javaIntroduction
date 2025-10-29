c23aeb99989fe5640283fc0a1a00d5c240e8b061
João Henrique <joaohenriquepf2005@gmail.com>
2025-10-29 10:50:12 -0300

    Update HelloWorld2.java to print song lyrics

--- fundamentos/HelloWorld2.java
+++ fundamentos/HelloWorld2.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        // Write your code here 💖
+
       System.out.println("I Walked through the door with you");
       System.out.println("the air was cold");
       System.out.println("But something about it felt like home somehow");
