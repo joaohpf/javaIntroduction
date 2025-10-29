678673c370bbbc0553f11d7f22f6e429c8cf700b
João Henrique <joaohenriquepf2005@gmail.com>
2025-10-29 14:28:15 -0300

    Create Example04.java

---
+++ fundamentos/control-flow/Example04.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+    
+    boolean isCatOwner = false;
+    boolean isDogOwner = true;
+
+    if(isCatOwner){
+      System.out.println("Use the code MEOW for 20% off cat items");
+    } else if (isDogOwner){
+      System.out.println("Use the code WOOF for 20% off dog items");
+    } 
+  }
+}
