5bb2512d197cea34419418b0e610c3dfb0e33945
João Henrique <joaohenriquepf2005@gmail.com>
2025-10-29 11:07:32 -0300

    Add HelloWorld class to display ID card information

---
+++ fundamentos/variaveis/Example02.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        String name = "Joao Henrique";
+        String state = "Goias";
+        char gender = 'M';
+        int age = 20;
+        boolean  organDonor = false;
+
+    System.out.println("ID CARD ");
+    System.out.println("nome: " + name);
+    System.out.println("estado: " + state);
+    System.out.println("genero: " +gender);
+    System.out.println("Idade: " + age);
+    System.out.println("Doador de Orgaos: " + organDonor);
+    }
+}
