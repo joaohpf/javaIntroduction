d2d95ecbe75758ae3d1e550c1ede38f60bfdb9d0
João Henrique <joaohenriquepf2005@gmail.com>
2025-10-29 14:52:48 -0300

    Add HelloWorld class with ride eligibility check

---
+++ fundamentos/control-flow/Example05.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+    
+    int weight = 41;
+    int height = 150;
+
+    if(weight >= 40 && height >=120){
+      System.out.println("Congrats! You can ride!");
+    } else {
+      System.out.println("Sorry, U Can't ride");
+    }
+        
+    }
+}
