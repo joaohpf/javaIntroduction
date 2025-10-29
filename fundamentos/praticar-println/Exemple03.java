9303256e644df7e5c9fd4290c266cb7eacf60e2a
João Henrique <joaohenriquepf2005@gmail.com>
2025-10-29 11:02:11 -0300

    Add HelloWorld class to print music tracks
    Prints a list of music tracks in the console.

---
+++ fundamentos/praticar-println/Exemple03.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("========== 🎵 Music Wrapped 🎵 ==========");
+
+        System.out.println("Leans, Pt.2 "); // Yunk Vino
+        System.out.println("Timeless "); // The Weeknd
+        System.out.println("Dreamcathcer "); // Travis Scott
+        System.out.println("TKN "); // Rosalia
+        System.out.println("THE BOX MEDLEY 10"); // The Box, Oruam, Borges, Meno Toddy, NGC Daddy, Pedrin and JS
+
+        System.out.println("======= 🎵 🎵 🎵 🎵 🎵 🎵 🎵 🎵 ========");
+    }
+}
