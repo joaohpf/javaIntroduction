3cdf3fec1c9087c3e46fe0e686f413ff6faf5482
João Henrique <joaohenriquepf2005@gmail.com>
2025-10-29 11:06:36 -0300

    Add Example01.java to display room item counts

---
+++ fundamentos/variaveis/Example01.java
@@ -0,0 +1,12 @@
+public class Example01 {
+    public static void main(String[] args) {
+      
+        int livingRoom = 15;
+        int masterBedroom = 45;
+        int bathRoom = 10;
+      
+    System.out.println("The Living Room box has " + livingRoom + " items");
+    System.out.println("The Master Bedroom box has " + masterBedroom + " items");
+    System.out.println("The Bathroom has " + bathRoom + " items");
+    }
+}
