64d3ad07f7431096cf6f783565108897fb81960f
João Henrique <joaohenriquepf2005@gmail.com>
2025-10-29 11:01:10 -0300

    Add HelloWorld class with println statements

---
+++ fundamentos/praticar-println/Exemple02.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+      System.out.println("I Walked through the door with you");
+      System.out.println("the air was cold");
+      System.out.println("But something about it felt like home somehow");
+    }
+}
