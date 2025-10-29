3cb105952a4bada2d21c0fb8a863edfbd1449204
João Henrique <joaohenriquepf2005@gmail.com>
2025-10-29 14:07:57 -0300

    Add HelloWorld class with pet store discount message

---
+++ fundamentos/control-flow/Example02.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+      
+      boolean isCatOwner = false;
+    
+    if (isCatOwner){
+      System.out.println("20% off select cat items with code MEOW2025");
+    } else {
+      System.out.println("Welcome to the Pets store!");
+    }
+    }
+}
